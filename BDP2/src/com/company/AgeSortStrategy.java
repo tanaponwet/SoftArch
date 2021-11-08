fad0cf877df6969afee81c0b6f877e156429d879
Tanapon W <77430053+tanaponwet@users.noreply.github.com>
2021-11-08 16:45:38 +0700

    Behavior Design Patterns 2

---
+++ BDP2/src/com/company/AgeSortStrategy.java
@@ -0,0 +1,10 @@
+package com.company;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class AgeSortStrategy implements SortStrategy {
+    // YOU MAY ADD UP TO 5 LINES OF CODE BELOW THIS COMMENT !! A LINE OF CODE MAY CONTAIN UP TO ONE SEMI-COLON !!
+    public void customSort(List<Person> people) { Collections.sort(people, Comparator.comparing(Person::getAge)); }
+}
---
+++ BDP2/src/com/company/SortStrategy.java
@@ -0,0 +1,8 @@
+package com.company;
+
+// DO NOT MODIFY ANYTHING BELOW THIS LINE!!
+import java.util.List;
+
+public interface SortStrategy {
+    void customSort(List<Person> people);
+}
